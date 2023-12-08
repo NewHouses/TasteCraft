1c3e4131a4544c7476d5a842157ae9d745a399f8
NewHouses <sergio.simons.c@gmail.com>
2023-12-08 23:48:15 +0100

    method for cleaning database

--- go-tastecraft/db/models/models.go
+++ go-tastecraft/db/models/models.go
@@ -1,6 +1,11 @@
 package models
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
 
 // DBModel is the type for database connection values
 type DBModel struct {
@@ -19,4 +24,72 @@ func NewModels(db *sql.DB) Models {
 			DB: db,
 		},
 	}
+}
+
+func (m *DBModel) CleanDB() error {
+	stmt := `
+	TRUNCATE steps;
+	`
+	_, err := m.DB.Exec(stmt)
+	if err != nil {
+		return err
+	}
+
+	stmt = `
+	TRUNCATE properties;
+	`
+	_, err = m.DB.Exec(stmt)
+	if err != nil {
+		return err
+	}
+
+	stmt = `
+	TRUNCATE ingredients;
+	`
+	_, err = m.DB.Exec(stmt)
+	if err != nil {
+		return err
+	}
+
+	stmt = `
+	DELETE FROM foods WHERE food_id != 0;
+	`
+	_, err = m.DB.Exec(stmt)
+	if err != nil {
+		return err
+	}
+
+	stmt = `
+	DELETE FROM recipes WHERE recipe_id != 0;
+	`
+	_, err = m.DB.Exec(stmt)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (m *DBModel) LastID(idColumn, tableName string) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var lastID *int
+
+	query := fmt.Sprintf(`
+	SELECT 
+		MAX(%s)
+	FROM 
+		%s;
+	`, idColumn, tableName)
+
+	row := m.DB.QueryRowContext(ctx, query)
+	err := row.Scan(
+		&lastID,
+	)
+
+	if lastID == nil {
+		return 0, err
+	}
+	return *lastID, err
 }
